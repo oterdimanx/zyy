bc7a0716aa88ddcc2a90f59cebcfdef5eb6303b6
Olivier Terdiman <olivier.terdiman@gmail.com>
2024-10-08 20:06:29 +0200

    firebase settings delete

--- src/utils/Firebase.ts
+++ src/utils/Firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcdzlZMD01e-x9teKIWJD4Rj8IymOLbiA",
-  authDomain: "socialapp-9b83f.firebaseapp.com",
-  projectId: "socialapp-9b83f",
-  storageBucket: "socialapp-9b83f.appspot.com",
-  messagingSenderId: "635004374736",
-  appId: "1:635004374736:web:b02daf40b9b19856f9d8d5",
-  measurementId: "G-PS5QJC1GDC"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app , "gs://socialapp-9b83f.appspot.com");
+export const storage = getStorage(app , "");
